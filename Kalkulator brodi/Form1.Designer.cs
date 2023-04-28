0deaa716af44bcc97c522c57ed799345a5d37d3d
Alvito Dwinovan <90097108+alvitodw@users.noreply.github.com>
2023-04-28 18:22:23 +0700

    Add files via upload

---
+++ Kalkulator brodi/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿
+namespace Kalkulator_brodi
+{
+    partial class History
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.utton3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.Operate = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.reset = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Operation_Process_Cell = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Result_Column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // utton3
+            // 
+            this.utton3.Location = new System.Drawing.Point(181, 142);
+            this.utton3.Name = "utton3";
+            this.utton3.Size = new System.Drawing.Size(34, 23);
+            this.utton3.TabIndex = 2;
+            this.utton3.Text = "3";
+            this.utton3.UseVisualStyleBackColor = true;
+            this.utton3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(101, 171);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(34, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(141, 171);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(34, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(181, 171);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(34, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(101, 200);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(34, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(141, 200);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(34, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(181, 200);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(34, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(101, 229);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(34, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // equal
+            // 
+            this.equal.Location = new System.Drawing.Point(141, 229);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(74, 23);
+            this.equal.TabIndex = 10;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            this.equal.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(236, 142);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(45, 23);
+            this.plus.TabIndex = 11;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(236, 171);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(45, 23);
+            this.minus.TabIndex = 12;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Location = new System.Drawing.Point(236, 200);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(45, 23);
+            this.multiply.TabIndex = 13;
+            this.multiply.Text = "x";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // divide
+            // 
+            this.divide.Location = new System.Drawing.Point(236, 229);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(45, 23);
+            this.divide.TabIndex = 14;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // Operate
+            // 
+            this.Operate.Location = new System.Drawing.Point(225, 113);
+            this.Operate.Name = "Operate";
+            this.Operate.Size = new System.Drawing.Size(56, 23);
+            this.Operate.TabIndex = 15;
+            this.Operate.Text = "Operate";
+            this.Operate.UseVisualStyleBackColor = true;
+            this.Operate.Click += new System.EventHandler(this.Operate_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Plus +",
+            "Minus -",
+            "Multiply *",
+            "Divide /"});
+            this.comboBox1.Location = new System.Drawing.Point(101, 115);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(114, 21);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
+            this.textBox1.TabIndex = 17;
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(100, 258);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(181, 23);
+            this.reset.TabIndex = 19;
+            this.reset.Text = "RESET";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Operation_Process_Cell,
+            this.Result_Column});
+            this.dataGridView1.Location = new System.Drawing.Point(304, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 45;
+            this.dataGridView1.Size = new System.Drawing.Size(278, 192);
+            this.dataGridView1.TabIndex = 22;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Operation_Process_Cell
+            // 
+            this.Operation_Process_Cell.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Operation_Process_Cell.HeaderText = "Operation Process";
+            this.Operation_Process_Cell.Name = "Operation_Process_Cell";
+            this.Operation_Process_Cell.ReadOnly = true;
+            // 
+            // Result_Column
+            // 
+            this.Result_Column.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Result_Column.HeaderText = "Result";
+            this.Result_Column.Name = "Result_Column";
+            this.Result_Column.ReadOnly = true;
+            // 
+            // History
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Operate);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.utton3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "History";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button utton3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button equal;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button Operate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button reset;
+        protected System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Operation_Process_Cell;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Result_Column;
+    }
+}
+
---
+++ Kalkulator brodi/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator_brodi
+{
+    public partial class History : Form
+    {
+        public string operationIs { get; set; } = "";
+
+        public History()
+        {
+            InitializeComponent();
+        }
+
+        public float number1 { get; set; } = 0;
+        public float number2 { get; set; } = 0;
+        public float result { get; set; } = 0;
+        private bool sudahdihitung;
+        private bool bukancombobox;
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (sudahdihitung)
+            {
+                textBox1.Clear();
+                sudahdihitung = false;
+            }
+            textBox1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (sudahdihitung)
+            {
+                textBox1.Clear();
+                sudahdihitung = false;
+            }
+            textBox1.Text +="2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (sudahdihitung)
+            {
+                textBox1.Clear();
+                sudahdihitung = false;
+            }
+            textBox1.Text +="3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (sudahdihitung)
+            {
+                textBox1.Clear();
+                sudahdihitung = false;
+            }
+            textBox1.Text +="4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (sudahdihitung)
+            {
+                textBox1.Clear();
+                sudahdihitung = false;
+            }
+            textBox1.Text +="5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (sudahdihitung)
+            {
+                textBox1.Clear();
+                sudahdihitung = false;
+            }
+            textBox1.Text +="6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (sudahdihitung)
+            {
+                textBox1.Clear();
+                sudahdihitung = false;
+            }
+            textBox1.Text +="7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (sudahdihitung)
+            {
+                textBox1.Clear();
+                sudahdihitung = false;
+            }
+            textBox1.Text +="8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (sudahdihitung)
+            {
+                textBox1.Clear();
+                sudahdihitung = false;
+            }
+            textBox1.Text +="9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (sudahdihitung)
+            {
+                textBox1.Clear();
+                sudahdihitung = false;
+            }
+            textBox1.Text +="0";
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+                operationIs += "+";
+            bukancombobox = true;
+                number1 = float.Parse(textBox1.Text);
+                textBox1.Clear();
+
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+                operationIs += "-";
+            bukancombobox = true;
+            number1 = float.Parse(textBox1.Text);
+                textBox1.Clear();
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+                operationIs += "*";
+            bukancombobox = true;
+            number1 = float.Parse(textBox1.Text);
+                textBox1.Clear();
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+                operationIs += "/";
+            bukancombobox = true;
+            number1 = float.Parse(textBox1.Text);
+                textBox1.Clear();
+        }
+
+        private void equal_Click(object sender, EventArgs e)
+        {
+            number2 = float.Parse(textBox1.Text);
+
+            if (operationIs.Equals("+"))
+                result = number1 + number2;
+
+            if (operationIs.Equals("-"))
+                result = number1 - number2;
+
+            if (operationIs.Equals("*"))
+                result = number1 * number2;
+
+            if (operationIs.Equals("/"))
+                result = number1 / number2;
+
+            textBox1.Text = $"{result}";
+            dataGridView1.Rows.Add($"{number1} {operationIs} {number2}", result);
+            sudahdihitung = true;
+            if (bukancombobox)
+            {
+                operationIs = "";
+            }
+            
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            dataGridView1.Rows.Clear();
+            result = (0);
+            number1 = (0);
+            number2 = (0);
+            operationIs = "";
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            if (comboBox1.SelectedIndex > 0) ;
+            string gettext = comboBox1.SelectedItem.ToString();
+            
+            if (gettext==("Plus +"))
+                {
+                operationIs = "+";
+                }   
+
+            if (gettext==("Minus -")) 
+                {
+                operationIs = "-";
+                }
+
+            if (gettext ==("Multiply *")) 
+                {
+                operationIs = "*";
+                }
+
+            if (gettext ==("Divide /")) 
+                {
+                operationIs = "/";
+                }
+            bukancombobox = false;
+        }
+
+        private void Operate_Click(object sender, EventArgs e)
+        {
+                number1 = float.Parse(textBox1.Text);
+                textBox1.Clear();
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
